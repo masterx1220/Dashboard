f16bbb4da0160d269d74bc9ac05daf6290631b43
masterx1220 <jstroz1220@gmail.com>
2023-10-29 19:25:58 -0500

    added basic style and scripting

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature`)
-.then(res => res.json())
-.then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.regular})`
-    console.log(data.urls)
+const search = document.getElementById("backgroundSearch")
+const searchBtnEl = document.getElementById("searchBtn")
+
+searchBtnEl.addEventListener("click", function(e){
+    let searchValue = search.value
+    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${searchValue}`)
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        // console.log(data.urls)
+        console.log(searchValue)
+    })
 })
