412f6a736da5778f59b29a259cd6730ca4e85a92
masterx1220 <jstroz1220@gmail.com>
2023-10-30 01:12:17 -0500

    updates
    JS and CSS updats

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const search = document.getElementById("backgroundSearch")
 const searchBtnEl = document.getElementById("searchBtn")
 
+window.onload = getPhoto()
+
 searchBtnEl.addEventListener("click", function(e){
     e.preventDefault()
     let searchValue = search.value
-    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${searchValue}`)
+    getRandomPhoto(searchValue)
+    
+})
+
+function getPhoto(){
+    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=random`)
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").innerHTML = `
-            <p>Photographer: ${data.user.username}</p>
+            <p class="bottom-left">Photographer: ${data.user.username}</p>
+        `
+    })
+}
+
+function getRandomPhoto(search){
+    fetch(`https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=${search}`)
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById("author").innerHTML = `
+            <p class="bottom-left">Photographer: ${data.user.username}</p>
         `
         // console.log(data.urls)
         console.log(searchValue)
     })
-})
+}
