dcf72ef4169cd63dded3e11e246422860b34459d
masterx1220 <jstroz1220@gmail.com>
2023-11-01 00:34:39 -0500

    Updated HTML, CSS and JS
    Added a date and time feature that updates every second.
    
    deleted unnecessary code in CSS
    
    updated HTML with a new div containing the time and date

--- index.js
+++ index.js
@@ -29,7 +29,28 @@ function getRandomPhoto(search){
         document.getElementById("author").innerHTML = `
             <p class="bottom-left">Photographer: ${data.user.username}</p>
         `
-        // console.log(data.urls)
         console.log(searchValue)
     })
 }
+
+function getCurrentTime(){
+    const now = new Date()
+    let current_Time = now.toLocaleTimeString("en-US", { timeStyle: "short" })
+    const day = now.getDay()
+    const daysOfTheWeek = {
+        1 : "Monday",
+        2 : "Tuesday",
+        3 : "Wednesday",
+        4 : "Thurday",
+        5 : "Friday",
+        6 : "Saturday",
+        7 : "Sunday"
+    }
+    let current_day = daysOfTheWeek[day]
+    document.getElementById("time-container").innerHTML = `
+    <h2 id="day">Today is ${current_day}</h2>
+    <h1 id="time">${current_Time}</h1>
+    `
+}
+
+setInterval(getCurrentTime,1000)
